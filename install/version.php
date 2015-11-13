92f89f11e3376a4a0423fb49f8ab822ad6fa114f
Oleg Maksimenko <oleg.39style@gmail.com>
2015-11-13 03:15:33 +0200

    Версия 1.1.0
    - Обновление библиотеки scssphp до версии 0.5.1
    - Обновлён класс \Olegpro\Csscompiler\SCSSCompiler. Добавлена поддержка
    окружения в классе \Olegpro\Csscompiler\SCSSCompiler. Для dev-окружения
    стили компилируются развёрнуто, с путём и строкой до исходного
    scss-файла. Dev-окружение определяется по принципу
    in_array(strtolower($_SERVER['ENV']), array('dev', 'demo'))

--- install/version.php
+++ install/version.php
@@ -1,5 +1,6 @@
-<?php
+<?
 $arModuleVersion = array(
-    "VERSION" => "1.0.3",
-    "VERSION_DATE" => "2015-02-15 23:46:21"
+	"VERSION" => "1.1.0",
+	"VERSION_DATE" => "2015-11-13 02:37:00"
 );
+?>
--- lib/scsscompiler.php
+++ lib/scsscompiler.php
@@ -4,19 +4,32 @@ namespace Olegpro\Csscompiler;
 
 require __DIR__ . '/../libs/scssphp/scss.inc.php';
 
-class SCSSCompiler extends Compiler {
+class SCSSCompiler extends Compiler
+{
 
     /**
-     * @var \scssc $scssphp
+     * @var \Leafo\ScssPhp\Compiler $scssphp
      */
     private $scssphp;
 
     /**
      * Constructor
-     * @return \Olegpro\Csscompiler\SCSSCompiler
      */
-    public function __construct() {     
-        $this->scssphp = new \scssc();
+    public function __construct()
+    {
+        $this->scssphp = new \Leafo\ScssPhp\Compiler();
+
+        $devEnvironment = (isset($_SERVER['ENV']) && in_array(strtolower($_SERVER['ENV']), array('dev', 'demo')));
+
+        $this->scssphp->setFormatter(
+            !$devEnvironment
+                ? '\Leafo\ScssPhp\Formatter\Crunched'
+                : '\Leafo\ScssPhp\Formatter\Expanded'
+        );
+
+        if ($devEnvironment) {
+            $this->scssphp->setLineNumberStyle(\Leafo\ScssPhp\Compiler::LINE_COMMENTS);
+        }
     }
 
     /**
@@ -24,8 +37,9 @@ class SCSSCompiler extends Compiler {
      * @param string $file path to file
      * @return string CSS
      */
-    public function toCss($file) {
-        $this->scssphp->setImportPaths(dirname($file));
+    public function toCss($file)
+    {
+        $this->scssphp->addImportPath(dirname($file));
         return ($css = @ file_get_contents($file)) !== false ? $this->scssphp->compile($css) : '';
     }
 
